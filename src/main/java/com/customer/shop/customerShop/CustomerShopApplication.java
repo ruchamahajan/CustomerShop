b444afc5bc42f0eb361b32b7116d695b83dd060b
Rucha Mahajan <rucha.mahajan@gmail.com>
2018-07-12 19:59:05 -0400

    first commit

---
+++ src/main/java/com/customer/shop/customerShop/CustomerShopApplication.java
@@ -0,0 +1,12 @@
+package com.customer.shop.customerShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerShopApplication.class, args);
+	}
+}
